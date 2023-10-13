8526b235f06c8ea7622efb2df55e27bc7b229b79
danielflorestello <64515144+danielflorestello@users.noreply.github.com>
2023-10-13 00:33:48 -0500

    commit third

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+LOGOUT_REDIRECT_URL = 'index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- myapp/views.py
+++ myapp/views.py
@@ -6,20 +6,26 @@ from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 @login_required
 def principal(request):
     return render(request, 'principal.html')
 
+
 @login_required
 def equipos(request):
     return render(request, 'equipos.html')
 
+
 @login_required
 def recomendacion(request):
     return render(request, 'recomendacion.html')
+
 
 def signup(request):
     if request.method == 'GET':
@@ -49,27 +55,22 @@ def signup(request):
             'form': UserCreationForm,
             'error': "Password do not match"
         })
- 
+
+
 def signin(request):
-    if request.method == 'GET':
-        return render(request, 'signin.html', {
-            'form': AuthenticationForm
+    user = authenticate(
+        request, username=request.POST['username'], password=request.POST['password'])
+
+    if user is None:
+        return render(request, 'index.html', {
+            'error': "Usuario o contraseña incorrecta"
         })
 
     else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
+        login(request, user)
+        return redirect('principal')
 
-        if user is None:
-            return render(request, 'signin.html', {
-                'form': AuthenticationForm,
-                'error': "Usuario o contraseña incorrecta"
-            })
 
-        else:
-            login(request, user)
-            return redirect('principal')
-        
 @login_required
 def signout(request):
     logout(request)
