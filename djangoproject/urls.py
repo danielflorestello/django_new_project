36dcd30834456fe2be43890a3a48984405e78010
danielflorestello <64515144+danielflorestello@users.noreply.github.com>
2023-12-11 23:56:56 -0500

    new commit

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -52,8 +52,6 @@ urlpatterns = [
 
     path('signin/', views.signin, name='signin'),
     path('logout/', views.signout, name='logout'),
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
